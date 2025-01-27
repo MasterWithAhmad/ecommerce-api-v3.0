4dc194fa4e99eabf5d5f8662638f0a3ce653ae26
Ahmad <ahmed.ibrahim.bp2019@gmail.com>
2025-01-27 08:54:56 +0300

    Refactored code to include middlewares and utils for better modularity and reusability

---
+++ middlewares/errorHandler.js
@@ -0,0 +1,11 @@
+// Middleware for centralized error handling
+const errorHandler = (err, req, res, next) => {
+    const statusCode = err.status || 500;
+    res.status(statusCode).json({
+      message: err.message || 'Internal Server Error',
+      stack: process.env.NODE_ENV === 'production' ? null : err.stack,
+    });
+  };
+  
+  module.exports = errorHandler;
+  
--- server.js
+++ server.js
@@ -1,18 +1,23 @@
-const express = require("express");
-const dotenv = require("dotenv");
-const connectDB = require("./database/config");
-const authRoutes = require("./routes/authRoute");
+const express = require('express');
+const dotenv = require('dotenv');
+const connectDB = require('./database/config');
+const authRoute = require('./routes/authRoute');
+const errorHandler = require('./middlewares/errorHandler');
 
 dotenv.config();
-connectDB();
 
 const app = express();
-
 app.use(express.json());
 
-// Routes
-app.use("/api/auth", authRoutes);
+// Database connection
+connectDB();
 
+// Routes
+app.use('/api/auth', authRoute);
+
+// Error handler
+app.use(errorHandler);
+
+// Server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-
